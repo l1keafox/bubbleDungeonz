dbbb9a9213cd276941546e9da2da0ede97fc4a67
Dragon Lore <tate.j.foster@gmail.com>
2022-10-11 15:41:50 -0600

    set up more for settingsSchema, added friend key to User, brought settings into index

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const Channel = require("./Chat/Channel");
 const User = require("./User");
 const Score = require("./Score");
+const Settings = require("./Settings");
 
 module.exports = { Channel, User, Score };

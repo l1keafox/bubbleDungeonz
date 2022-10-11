2e0c6207c5b7ba1339d8fc17359b5b0d46c5d96a
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-10-10 22:03:38 -0600

    accidentally deleted a word

--- server/models/User.js
+++ server/models/User.js
@@ -28,7 +28,7 @@ const userSchema = new Schema(
     }
 );
 
-Schema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
       const saltRounds = 10;
       this.password = await bcrypt.hash(this.password, saltRounds);

5003432e8f62e4b7afe2963c04ba69192ff8df56
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-10 21:47:04 -0600

    created/cleaned client auth/mutation/queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,10 +1,10 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
         name
       }
@@ -16,7 +16,7 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
         name
       }

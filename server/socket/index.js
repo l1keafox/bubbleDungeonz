63c1639ab2e44dc9e9b2db4a32b601f8cabca11f
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-11 16:38:04 -0600

    Bubbles down emites in io

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,17 +2,17 @@
 // Init should be done in server.js, there are several steps needed with express too initate.
 // Once it is initated, you can use this module to grab the io.
 
-let io; // This will hold the io server.
+//var io; // This will hold the io server.
 
 const initIo = (app) => {
   const http = require("http");
   const ioServer = http.createServer(app);
-  const socketio = require("socket.io");
+  const {Server} = require("socket.io");
   const cors = require("cors"); // This is used for socket.io
 
   // This is cheating, I wish there was an way
   // to grab this server that is created.
-  io = socketio(ioServer, {
+  global.io = new Server(ioServer, {
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
@@ -20,7 +20,6 @@ const initIo = (app) => {
   });
 
   // Just a connection based.
-
   io.on("connection", (socket) => {
     console.log("a user connected");
     socket.on("disconnect", () => {
@@ -29,9 +28,8 @@ const initIo = (app) => {
   });
 
   // Or we add all socket code here? Mmmm not too bad of an idea, because we can use this folder to store all socket stuff.
-
-  console.log("  -IO> server is initalized");
+  console.log("  -IO> server is initalized, set to global.io");
   return ioServer;
 };
 
-module.exports = { initIo, io }; // was trying to make it so io can be exported and used else where, but for waht ever reason this fails.
+module.exports = { initIo }; // was trying to make it so io can be exported and used else where, but for waht ever reason this fails.

4bd42d0d31f8e39fe160b6047c680e6606b3e145
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-10-10 21:35:08 -0600

    apollo server started

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const { User } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -7,7 +9,35 @@ const resolvers = {
         }
     },
   
-    // Mutation: {
-    // },
-  };
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+          const user = await User.create({ name, email, password });
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No profile with this email found!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+        removeUser: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOneAndDelete({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+  },
+}
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,10 +6,20 @@ type User {
     username: String
     email:String
 }
+type Auth {
+    token: ID!
+    user:User
+}
 
 type Query{
     users: [User]
 }
+type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    removeUser: User
+  }
 `;
 
 module.exports = typeDefs;

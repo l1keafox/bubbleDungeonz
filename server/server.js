08b71f11f5a30ebb0ce501fe6a6a669b040b39ff
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-11 07:53:35 -0600

    added modularized io

--- server/server.js
+++ server/server.js
@@ -6,13 +6,24 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+
 const PORT = process.env.PORT || 3001;
+
+
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
+
+const cors = require("cors"); // This is used for socket.io
+const ioPORT = process.env.PORT+1 || PORT+1; // The +1 to ioPort is questionable.
+const {initIo} = require('./socket/index'); // initIo to initalize the server, io later on just to grab the object.
+const ioServer = initIo(app, cors); // initalizing io into serverIo
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -25,7 +36,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -36,6 +46,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
+    ioServer.listen(ioPORT, () => {
+      console.log(
+        `Socket.io listening on http://localHost:${ioPORT}?`
+      );
+    });
+
   })
   };
   
---
+++ server/socket/index.js
@@ -0,0 +1,24 @@
+// This is modular! Meaning you need to import this to use,
+// Init should be done in server.js, there are several steps needed with express too initate.
+// Once it is initated, you can use this module to grab the io.
+
+let io; // This will hold the io server.
+
+const initIo = (app, cors) => {
+  const http = require("http");
+  const ioServer = http.createServer(app);
+  const socketio = require("socket.io");
+
+  // This is cheating, I wish there was an way
+  // to grab this server that is created.
+  io = socketio(ioServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+  return ioServer;
+
+};
+
+module.exports = { initIo, io };

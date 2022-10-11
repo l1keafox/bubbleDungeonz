9a3320972c38696b1e2fdbdbe22dcc865aa6ee14
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-10 21:56:17 -0600

    userrname changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,24 +1,26 @@
 import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
-  mutation addUser($name: String!, $email: String!, $password: String!) {
-    addUser(name: $name, email: $email, password: $password) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $name, email: $email, password: $password) {
       token
       user {
         _id
-        name
+        username
+        email
+        password
       }
     }
   }
 `;
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       token
       user {
         _id
-        name
+        username
       }
     }
   }

0785e28c79aca2ddfb8b3e53cd421393a47d07e2
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-11 14:05:58 -0600

    Folder structs/index.js

---
+++ server/engine/index.js
@@ -0,0 +1,25 @@
+/* The ENGINE
+
+Everything in the engine is done in virutal memory (just normal vars nothing saved in the database),
+This will through FPS 
+
+
+
+
+*/
+
+const FRAMES = 60; // Number of frames per sec
+const FramePerLoop = FRAMES/1000; // number of cycles per second. so every 16.67MS there will be an game loop.
+
+const initEngine = {
+    // a function to initalize the engine
+
+};
+const engine = {
+    // this returns various methods too
+    addGame: (game) => {
+
+    },
+}
+
+module.exports = { initEngine, engine };

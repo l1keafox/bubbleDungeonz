9c2d2a7527bd71d787bbf8fc4a7f9629c162c0c4
relero90 <relero90@gmail.com>
2022-10-12 13:41:48 -0600

    repaired login functionality for landing page

---
+++ client/src/pages/Games/Menu/GamesPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Canvas from "../../../components/Canvas/Canvas.js";
+
+function GamesMenu() {
+  return (
+    <div className="gameViewContainer">
+      <Canvas />
+    </div>
+  );
+}
+
+export default GamesMenu;

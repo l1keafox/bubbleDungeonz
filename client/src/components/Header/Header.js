d7840b41ba6e835ba58b1b8fa207b633938086ca
madrodgerflynn <madrodgerflynn@gmail.com>
2022-10-12 14:54:28 -0600

    got toggle to work needs right path

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,20 +3,28 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import { FaHamburger } from "react-icons/fa";
 import auth from "../../utils/auth";
+import Settings from "../Settings/Settings";
 
 function Header() {
   //state variable to control whether the header has a Login or Logout link
   const [logInOrOut, setLogInOrOut] = useState("Login");
-  const openSettings = (e) => {
-    e.preventDefault();
-    console.log("here?")
+  const [showModal, changeModal] = useState(false);
+  const toggleModal = () => {
+    if (!showModal) {
+      changeModal(true);
+    } else {
+      changeModal(false);
+    }
+    console.log("toggleModal", showModal);
   };
-
   //need logic to conditionally render Login/Logout link based on user objects loggedIn state
 
   return (
     <div className="headerDiv">
       <h1 className="headerTitle">App Title</h1>
+      <Settings show={showModal}>
+        <p>Settings</p>
+      </Settings>
       <ul className="navBar">
         <Link to={{ pathname: "/" }} className="navLink">
           <li>Home</li>
@@ -24,9 +32,14 @@ function Header() {
         <Link to={{ pathname: "/games" }} className="navLink">
           <li>Games</li>
         </Link>
-        <li className={ auth.loggedIn() ? "navLink":"navLink hidden" } onClick= {auth.logout} >Logout</li> 
+        <li
+          className={auth.loggedIn() ? "navLink" : "navLink hidden"}
+          onClick={auth.logout}
+        >
+          Logout
+        </li>
         <li className="navLink">
-          <FaHamburger onClick={ openSettings }></FaHamburger>
+          <FaHamburger onClick={toggleModal}></FaHamburger>
         </li>
       </ul>
     </div>
--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -1,7 +1,9 @@
+import { FaHamburger } from "react-icons/fa";
 import "./Settings.css";
 // import "./colors.css";
-
-function Settings() {
+import Header from "../Header/Header";
+function Settings({ handleClose, show, children }) {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
   const handleFormSubmit = (e) => {
     e.preventDefault();
   };
@@ -23,61 +25,73 @@ function Settings() {
     "Navy",
     "Purple",
     "White",
-    "Black"
+    "Black",
   ];
 
   return (
-    <div
-      className="modal fade"
-      id="exampleModal"
-      tabIndex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="settings">
-              Settings
-            </h1>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <select className="form-select" aria-label="Screen-text-color" defaultValue={"Fuscia"}>
-              <option>Screen Text Color</option>
-              {
-                colorKeys.map( (opt) => (
+    (
+      <div className={showHideClassName}>
+        <button
+          type="button"
+          className="btn launch-button"
+          data-bs-toggle="modal"
+          data-bs-target="#launchModal"
+        ></button>
+      </div>
+    ),
+    (
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="settings">
+                Settings
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <select
+                className="form-select"
+                aria-label="Screen-text-color"
+                defaultValue={"Fuscia"}
+              >
+                <option>Screen Text Color</option>
+                {colorKeys.map((opt) => (
                   <option key={opt}>opt</option>
-                ))
-              }
-
-            </select>
-            
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleFormSubmit}
-            >
-              Save changes
-            </button>
+                ))}
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleFormSubmit}
+              >
+                Save changes
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 export default Settings;

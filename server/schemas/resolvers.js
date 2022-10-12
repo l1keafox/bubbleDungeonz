84a4bb7c1c16feeac35a7485c1d1d2c4329bed08
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-10-12 10:07:44 -0600

    added a ME route that returns the user info when called (when the user is logged in)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -56,8 +56,14 @@ const resolvers = {
             return;
           }
           return channel;
-        
-        }
+        },
+        //returns the current user id, must be logged in for it to work.
+        me: async (parent, args, context) => {
+          if (context.user) {
+            return Profile.findOne({ _id: context.user._id });
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
 
     },
   

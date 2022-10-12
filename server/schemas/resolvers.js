c3f8095f9a0411327d63498d8e52644ecf08cb12
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-10-12 10:10:20 -0600

    fixed typo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -60,7 +60,7 @@ const resolvers = {
         //returns the current user id, must be logged in for it to work.
         me: async (parent, args, context) => {
           if (context.user) {
-            return Profile.findOne({ _id: context.user._id });
+            return User.findOne({ _id: context.user._id });
           }
           throw new AuthenticationError('You need to be logged in!');
         },

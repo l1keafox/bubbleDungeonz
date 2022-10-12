108330a2d88a75f55495afc9a6f1dd28786572aa
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-10-12 12:39:36 -0600

    firstgard not working

--- client/src/pages/Games/GamesPage.js
+++ client/src/pages/Games/GamesPage.js
@@ -1,13 +1,24 @@
-
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Canvas from "../../components/Canvas/Canvas.js";
 
 function GamesPage() {
-
+  //{/* <Canvas/>  */}
   return (
     <div className="gameViewContainer">
-      <Canvas/> 
+      <div style={{ width: "18rem" }}>
+        {/* <img src="..." alt="..." /> */}
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
       </div>
+    </div>
   );
 }
 

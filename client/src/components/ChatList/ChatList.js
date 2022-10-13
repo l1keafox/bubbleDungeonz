fde2faecec8d4e0b3ba9bded613ec3d018fa8cf4
relero90 <relero90@gmail.com>
2022-10-12 18:32:54 -0600

    chat component appears as a fixed feature in bottom right of screen; links to channels display as horizontally aligned buttons

--- client/src/components/ChatList/ChatList.js
+++ client/src/components/ChatList/ChatList.js
@@ -1,40 +1,44 @@
 import "./ChatList.css";
 import React, { useEffect, useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
-import { GET_USER_CHANNELS,GET_ALL_CHANNELS } from "../../utils/queries";
+import { GET_USER_CHANNELS, GET_ALL_CHANNELS } from "../../utils/queries";
 import ChatWindow from "../ChatWindow/ChatWindow";
 import auth from "../../utils/auth";
 import { useExistingUserContext } from "../../utils/existingUserContext";
 
-export default function ChatList(){
-    const {loading,data} = useQuery(GET_USER_CHANNELS);
-    const [openChannelIds,setOpenChannelIds] = useState([]);
-    const channels = data?.memberChannels || [];
+export default function ChatList() {
+  const { loading, data } = useQuery(GET_USER_CHANNELS);
+  const [openChannelIds, setOpenChannelIds] = useState([]);
+  const channels = data?.memberChannels || [];
 
-    function channelOptions({channels}){
-        if(loading){
-            return <p>loading</p>
-        }else{
-            return channels.map((item)=><li onClick={() => openChannel(item._id,item.channelName)} key={item._id}>{item.channelName}</li>);
-        }
-        
+  function channelOptions({ channels }) {
+    if (loading) {
+      return <p>loading</p>;
+    } else {
+      return channels.map((item) => (
+        <p
+          onClick={() => openChannel(item._id, item.channelName)}
+          key={item._id}
+          className="channelLink"
+        >
+          {item.channelName}
+        </p>
+      ));
     }
-    function openChannel(id,name){
-        if(!openChannelIds.includes(id)){
-            setOpenChannelIds([...openChannelIds,id])
-        }
+  }
+  function openChannel(id, name) {
+    if (!openChannelIds.includes(id)) {
+      setOpenChannelIds([...openChannelIds, id]);
     }
-    function loadedChannels(list){
-        return list.map((item)=><ChatWindow key={item} channelId={item}/>)
-    }
+  }
+  function loadedChannels(list) {
+    return list.map((item) => <ChatWindow key={item} channelId={item} />);
+  }
 
-    return(
-        <aside>
-            <ul>
-                {channelOptions({channels})}
-            </ul>
-            {loadedChannels(openChannelIds)}
-        </aside>
-    );
-
+  return (
+    <aside className="chatAside">
+      <div className="chatChannelsList">{channelOptions({ channels })}</div>
+      {loadedChannels(openChannelIds)}
+    </aside>
+  );
 }

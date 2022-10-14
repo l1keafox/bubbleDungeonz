6d2653bdb0cb84950efad5ecb31ea423f363bab7
relero90 <relero90@gmail.com>
2022-10-14 12:56:19 -0600

    query pulls game card for gameState game

--- client/src/pages/Games/Play/GamePlay.js
+++ client/src/pages/Games/Play/GamePlay.js
@@ -3,7 +3,7 @@ import Canvas from "../../../components/Canvas/Canvas.js";
 import { useGameContext } from "./../../../utils/gameContext";
 import "./GamePlay.css";
 import { useMutation, useQuery } from "@apollo/client";
-
+import { GET_GAME_CARDS } from "../../../utils/queries";
 
 import auth from "../../../utils/auth";
 import ChatList from "../../../components/ChatList/ChatList.js";
@@ -13,8 +13,6 @@ function GamePlay() {
   // changeTitle(gameState);
   let game;
 
-  
- 
   switch (gameState) {
     case "Bubble Trouble":
       game = <Canvas />;
@@ -23,43 +21,58 @@ function GamePlay() {
       game = <Canvas />;
       break;
   }
-  return (
-    <div className="gamePlayContainer">
-      <div className="canvasContainer">
-        <h1 className="gamePlayTitle">{gameState}</h1>
-        {game}
-        <p className="scoreCounter">
-          Current Score: <span className="currentScore"></span>
-        </p>
-      </div>
-      <div className="highScoreContainer">
-        <div className="featuredScoresDiv">
-          <div className="cardBody">
-            <h5 className="featuredGame card-title">{gameState} High Scores</h5>
-          </div>
-          <div className="featuredScoresList">
-            <div className="featuredScore">
-              <span className="featuredUsername">username</span> - score
+
+  // pulling game card data for game scores
+  const { loading, error, data } = useQuery(GET_GAME_CARDS);
+  console.log(data);
+  try {
+    const gameCards = data?.gameCards || [];
+    const currentGameCard = gameCards.filter(
+      (card) => card.title === gameState
+    );
+    console.log(currentGameCard);
+
+    return (
+      <div className="gamePlayContainer">
+        <div className="canvasContainer">
+          <h1 className="gamePlayTitle">{gameState}</h1>
+          {game}
+          <p className="scoreCounter">
+            Current Score: <span className="currentScore"></span>
+          </p>
+        </div>
+        <div className="highScoreContainer">
+          <div className="featuredScoresDiv">
+            <div className="cardBody">
+              <h5 className="featuredGame card-title">
+                {gameState} High Scores
+              </h5>
             </div>
-            <div className="featuredScore">
-              <span className="featuredUsername">username</span> - score
-            </div>
-            <div className="featuredScore">
-              <span className="featuredUsername">username</span> - score
-            </div>
-            <div className="featuredScore">
-              <span className="featuredUsername">username</span> - score
-            </div>
-            <div className="featuredScore">
-              <span className="featuredUsername">username</span> - score
+            <div className="featuredScoresList">
+              <div className="featuredScore">
+                <span className="featuredUsername">username</span> - score
+              </div>
+              <div className="featuredScore">
+                <span className="featuredUsername">username</span> - score
+              </div>
+              <div className="featuredScore">
+                <span className="featuredUsername">username</span> - score
+              </div>
+              <div className="featuredScore">
+                <span className="featuredUsername">username</span> - score
+              </div>
+              <div className="featuredScore">
+                <span className="featuredUsername">username</span> - score
+              </div>
             </div>
           </div>
         </div>
+        {auth.loggedIn() ? <ChatList /> : <div />}
       </div>
-      {auth.loggedIn() ? <ChatList /> : <div />}
-    </div>
-  );
+    );
+  } catch (err) {
+    if (err) console.log(err);
+  }
 }
-
 
 export default GamePlay;

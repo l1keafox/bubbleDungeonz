6d90dce5f1a93484cdbea04835384b201a9abcc8
relero90 <relero90@gmail.com>
2022-10-14 11:20:17 -0600

    featured scores populate on landing page (must have scores in database)

--- client/src/components/FeaturedScores/FeaturedScores.js
+++ client/src/components/FeaturedScores/FeaturedScores.js
@@ -7,88 +7,94 @@ import image from "../../pages/Games/Menu/assets/bubble-trouble-screenshot.png";
 import { GET_GAME_CARDS } from "./../../utils/queries";
 
 function FeaturedScores() {
-  const [featuredGame, setFeaturedGame] = useState();
+  // const [featuredGame, setFeaturedGame] = useState();
 
   // Query for all games
   const { loading, error, data } = useQuery(GET_GAME_CARDS); //async not functioning
   console.log(data);
-  // const gameCards = data.gameCards || [];
-  // console.log(gameCards);
-  if (loading) {
-    return <p>loading</p>;
-  } else {
-    console.log(data.gameCards);
 
-    let randomGameIndex = Math.floor(Math.random() * data.gameCards.length);
-    let randomGame = data.gameCards[randomGameIndex];
-    console.log(randomGame);
-    setFeaturedGame("test");
+  try {
+    const gameCards = data?.gameCards || [];
+    console.log(gameCards);
+
+    let randomGameIndex = Math.floor(Math.random() * gameCards.length);
+    // let featuredGame = gameCards[randomGameIndex];
+    let featuredGame = gameCards[0]; //until the system has more than one game
+
     console.log(featuredGame);
-  }
 
-  // Pick a game at random from the list
+    // Pick a game at random from the list
 
-  // array of all scores from featured game in descending order
-  const allScoresArray = featuredGame.scores
-    .map((score) => ({
-      score: score.score,
-      username: score.user.username,
-    }))
-    .sort((a, b) => a - b); //not successfully sorting the array members - they're objects :(
-  console.log(allScoresArray);
+    // array of all scores from featured game in descending order
+    const allScoresArray = featuredGame.scores
+      .map((score) => ({
+        score: score.score,
+        username: score.user.username,
+      }))
+      .sort((a, b) => a - b); //not successfully sorting the array members - they're objects :(
+    console.log(allScoresArray);
 
-  // reduced to 5 scores
-  let highScoresArray = [];
-  for (var i = allScoresArray.length - 1; i > allScoresArray.length - 6; i--) {
-    highScoresArray.push(allScoresArray[i]);
-  }
-  console.log(highScoresArray);
-  // console.log(highScoresArray.map((score) => score.score));
-
-  function populateHighScores() {
-    if (loading) {
-      return <p>loading</p>;
-    } else {
-      // list items of username - score
-      // map over highScoresArray
-      return highScoresArray.map((score) => (
-        <div className="featuredScore">
-          <span className="featuredUsername">{score.username}</span> -{" "}
-          {score.score}
-        </div>
-      ));
+    // reduced to 5 scores
+    let highScoresArray = [];
+    for (
+      var i = allScoresArray.length - 1;
+      i > allScoresArray.length - 6;
+      i--
+    ) {
+      highScoresArray.push(allScoresArray[i]);
     }
-  }
+    console.log(highScoresArray);
+    // console.log(highScoresArray.map((score) => score.score));
 
-  return (
-    <div>
-      <div className="featuredScoresDiv">
-        <img
-          className="card-img-top featuredGameImg"
-          src={image}
-          alt="Card image cap"
-        />
-        <div className="cardBody">
-          <h5 className="featuredGame card-title">Featured Game:</h5>
-          <h5 className="featuredGameTitle card-title">{featuredGame.title}</h5>
-        </div>
-        <hr
-          style={{
-            height: "1px",
-            width: "95%",
-            borderWidth: "0",
-            color: "yellow",
-            backgroundColor: "yellow",
-            margin: ".5rem",
-          }}
-        />
-        <div className="featuredScoresList">
-          <h5 className="highScores card-title">High Scores</h5>
-          {populateHighScores()}
+    function populateHighScores() {
+      if (loading) {
+        return <p>loading</p>;
+      } else {
+        // list items of username - score
+        // map over highScoresArray
+        return highScoresArray.map((score) => (
+          <div className="featuredScore">
+            <span className="featuredUsername">{score.username}</span> -{" "}
+            {score.score}
+          </div>
+        ));
+      }
+    }
+
+    return (
+      <div>
+        <div className="featuredScoresDiv">
+          <img
+            className="card-img-top featuredGameImg"
+            src={image}
+            alt="Card image cap"
+          />
+          <div className="cardBody">
+            <h5 className="featuredGame card-title">Featured Game:</h5>
+            <h5 className="featuredGameTitle card-title">
+              {featuredGame.title}
+            </h5>
+          </div>
+          <hr
+            style={{
+              height: "1px",
+              width: "95%",
+              borderWidth: "0",
+              color: "yellow",
+              backgroundColor: "yellow",
+              margin: ".5rem",
+            }}
+          />
+          <div className="featuredScoresList">
+            <h5 className="highScores card-title">High Scores</h5>
+            {populateHighScores()}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (err) {
+    if (err) console.log(err);
+  }
 }
 
 export default FeaturedScores;
